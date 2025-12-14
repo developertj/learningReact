3332fcb1433708597482444042720c3a580605b5
Tejas Bhanushali <tejasbhanushali.1@gmail.com>
2025-12-14 22:45:35 +0530

    Created my first component
    Created my first component using react and jsx. Also practiced React.createElement

---
+++ Lec3.5/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+
+//writing my first ever component
+function FirstComponent() {
+  return (
+    <>
+      <h1>This is my first component of react</h1>
+      <h2>Hi this is my header 2</h2>
+      h3
+    </>
+  );
+}
+
+function SecondComponent() {
+  // this is called as react fragment. also written as
+  <React.Fragment>
+    <h1>
+      This header is using react frangment. We cant apply styles to react
+      fragment as its an empty tag.
+    </h1>
+  </React.Fragment>;
+}
+
+root.render(<FirstComponent />);
